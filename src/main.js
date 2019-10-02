b8fc6d94a4ed19759d260bf3e705a8dea7e904e7
hghghhh <294105401@qq.com>
2019-10-02 20:46:40 +0800

    222

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import Vue from 'vue';
 import app from './App.vue';
 import VueRouter from 'vue-router';
 import router from './router.js';
+import vueResource from 'vue-resource';
 import 'mint-ui/lib/style.css';
 import '../lib/mui-master/dist/css/mui.min.css';
 import '../lib/mui-master/dist/css/icons-extra.css';
 
 
 Vue.use(VueRouter);
+Vue.use(vueResource);
 
 
-import { Header } from 'mint-ui';
+import { Header, Swipe, SwipeItem } from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 
 var vm = new Vue({
